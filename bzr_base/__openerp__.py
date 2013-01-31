874cd7aac3715e7a037c1ee672f5aa836eb20fe9
开阖软件 Jeff Wang <jeff@osbzr.com>
2013-02-01 01:22:46 +0800

    '基础模块和总账模块'

---
+++ bzr_base/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'bzrerp基础模块',
+    'version': '1.0',
+    'category': 'Hidden/Dependency',
+    'description': """
+    """,
+    'author': 'bzrerp team',
+    'depends': ['base'],
+    'website': 'https://launchpad.net/bzrerp',
+    'data': [],
+    'installable': True,
+    'auto_install': True,
+    'images': [],
+}
+
---
+++ bzr_fi_gl/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': '财务会计 - 总账',
+    'version': '1.0',
+    'category': 'Accounting & Finance',
+    'description': """
+
+""",
+    'author': 'bzrerp team',
+    'sequence': 1,
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'demo': [
+    ],
+    'test': [
+    ],
+    'css': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
