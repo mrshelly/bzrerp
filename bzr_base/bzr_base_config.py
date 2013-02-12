e517b1f3bd0a9d1d07fc1a822d3f2ecb015e1d8a
ccdos <ccdos@163.com>
2013-02-12 17:49:13 +0800

    中文字符串需要加 u 强制指定为 unicode 编码，否则在某些情况下会导致OE 报错。

--- bzr_base/bzr_base_config.py
+++ bzr_base/bzr_base_config.py
@@ -5,12 +5,12 @@ from openerp.osv import fields, osv
 #状态
 class bzr_state(osv.osv):
     _name='bzr.state'
-    _description='状态'
+    _description=u'状态'
     _columns={
-        'object':fields.char('对象',size=64),
-        'seq':fields.integer('序号'),
-        'key':fields.char('值',size=64),
-        'name':fields.char('文本',size=64),
+        'object':fields.char(u'对象',size=64),
+        'seq':fields.integer(u'序号'),
+        'key':fields.char(u'值',size=64),
+        'name':fields.char(u'文本',size=64),
     }
 def get_states(object):
     """
