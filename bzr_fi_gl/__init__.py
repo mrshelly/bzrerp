0664becc349de036c42af9270fe05afa537641e5
mrshelly <mrshelly@hotmail.com>
2013-02-27 20:55:08 +0800

    Update bzr_fi_gl/__init__.py

--- bzr_fi_gl/__init__.py
+++ bzr_fi_gl/__init__.py
@@ -1,8 +1,5 @@
 # -*- coding: utf-8 -*-
 
-<<<<<<< HEAD
-# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
-=======
 #配置数据
 import bzr_fi_gl_config
 
@@ -11,4 +8,4 @@ import bzr_fi_gl_master
 
 #业务数据
 import bzr_fi_gl_document
->>>>>>> osbzr/master
+
